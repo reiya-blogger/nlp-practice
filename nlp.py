8b54ecd6a8e23c06aa726cca58f905e55846cbc2
reiya <reiya.blogger1495@gmail.com>
2023-01-07 09:28:16 +0900

    自然言語処理の問題を追加

--- nlp.py
+++ nlp.py
@@ -3,3 +3,15 @@
 strings = 'stressed'
 string_reverse = strings[::-1]
 print(string_reverse)
+
+# 01. 「パタトクカシー」
+# 1,3,5,7文字目を取り出して連結
+
+a = 'パタトクカシー'
+print(a[0] + a[2] + a[4] + a[6])
+
+# 「パトカー」と「タクシー」の文字列を先頭から交互に連結して表示
+
+a = 'パトカー'
+b = 'タクシー'
+print(a[0] + b[0] + a[1] + b[1] + a[2] + b[2] + a[3] + b[3])

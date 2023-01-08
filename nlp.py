23f7dbc4af4942fcb7b2f661220d669e307f0856
reiya <reiya.blogger1495@gmail.com>
2023-01-08 10:51:32 +0900

    円周率の問題を追加

--- nlp.py
+++ nlp.py
@@ -15,3 +15,23 @@ print(a[0] + a[2] + a[4] + a[6])
 a = 'パトカー'
 b = 'タクシー'
 print(a[0] + b[0] + a[1] + b[1] + a[2] + b[2] + a[3] + b[3])
+
+# 03. 円周率
+
+# “Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics.”
+# という文を単語に分解し，各単語の（アルファベットの）文字数を先頭から出現順に並べたリストを作成せよ．
+
+import re
+
+sentence = 'Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics.'
+words = sentence.split()
+
+countlist = []
+
+for word in words:
+    numbers = len(word)
+    print(numbers)
+
+pi = countlist.insert(numbers, 0)
+
+print(pi)
